534a1dfbf82e332324c2a089c5e4e864b074d325
jackhkmatthews <jackhkmatthews@gmail.com>
2016-12-10 14:32:38 +0000

    base functionality of backing track, copy and paste lyrics from google, typing testing and styling reached

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(start);
 
 function start(){
   game.appendHtml();
+  game.makeSubmitListen();
+  game.makeKeysListen();
 }
 
 var game = {
@@ -17,6 +19,14 @@ var game = {
   $song: '',
 
   $button: '',
+
+  lyricsHtml: '',
+
+  paragraphyHtml: [],
+
+  paragraphyTesting: [],
+
+  currentCharacterIndex: 0,
 
   appendHtml: function appendHtml(){
     game.$html = game.createHtml();
@@ -58,8 +68,86 @@ var game = {
     e.preventDefault();
     console.log('music should pause');
     document.getElementById('song').pause();
+  },
+
+  makeSubmitListen: function makeSubmitListen (){
+    console.log($('#user-form'));
+    $('#submit').on('click', game.submitCallback);
+  },
+
+  submitCallback: function submitCallback(e){
+    e.target.preventDefault;
+    game.getLyrics();
+    game.displayLyrics();
+    game.convertLyrics();
+    game.currentCharacterIndex = 0;
+  },
+
+  getLyrics: function getLyrics(){
+    game.lyricsHtml = $('#user-text').val();
+  },
+
+  displayLyrics: function displayLyrics(){
+    var splitLyrics = game.lyricsHtml.split('<br>');
+    splitLyrics[0] = splitLyrics[0].replace('<p>', '');
+    splitLyrics.pop();
+
+    //inside each string of split lyrics
+    //create span element with id = currentCharacterIndex
+    //span.inner html = element of string
+
+    for (var i = 0; i < splitLyrics.length; i++) {
+      var line = splitLyrics[i];
+      for (var j = 0; j < line.length; j++) {
+        var span = document.createElement('span');
+        span.className = game.currentCharacterIndex;
+        span.innerHTML = line[j];
+        game.paragraphyHtml.push(span);
+        game.paragraphyTesting.push(span);
+        game.currentCharacterIndex += 1;
+      }
+      var lineBreak = document.createElement('br');
+      game.paragraphyHtml.push(lineBreak);
+    }
+
+    console.log(game.paragraphyHtml);
+
+
+    var $div = $(document.createElement('div'));
+    console.log(game.paragraphyHtml.toString());
+    $div.append(game.paragraphyHtml);
+    $('#game').prepend($div);
+
+    game.currentCharacterIndex = 0;
+
+  },
+
+  convertLyrics: function convertLyrics(){
+    var div = document.createElement('div');
+    div.innerHTML = game.lyricsHtml;
+    game.lyrics = div.textContent;
+  },
+
+  makeKeysListen: function makeKeysListen(){
+    $(window).on('keypress', game.testIfCorrectKey);
+  },
+
+  testIfCorrectKey: function testIfCorrectKey(e){
+    if (game.paragraphyTesting[game.currentCharacterIndex].innerHTML === String.fromCharCode(e.which)){
+      console.log('pass!');
+      var currentClass = '.' + [game.currentCharacterIndex];
+      $(currentClass).addClass('correct');
+      game.currentCharacterIndex += 1;
+    } else {
+      console.log('fails');
+    }
   }
+
 };
+
+
+
+
 
 //create play button
 //make play button listen for clicks
